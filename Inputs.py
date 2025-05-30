3da94354a875f132b0369f3d53e232bdd4d9fdfc
ramaarguello <ramaarguello17@gmail.com>
2025-05-29 23:08:39 -0300

    Delete Inputs.py

--- Inputs.py
+++
@@ -1,8 +0,0 @@
-array_nombres = ["Sofia", "Mateo", "Lucia", "Tomas", "Valentina"]
-array_notas_primer_jurado = [8, 9, 3, 6, 9]
-array_notas_segundo_jurado = [9, 8, 7, 6, 9]
-array_notas_tercer_jurado = [10, 10, 8, 7, 10]
-
-
-
-
